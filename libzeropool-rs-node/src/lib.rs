fb35fca3e2866e7c34765c55f4ec19cf9455a1a4
Dmitry Vdovin <voidxnull@gmail.com>
2021-08-11 04:49:30 +0200

    Make it compile

--- libzeropool-rs-node/src/lib.rs
+++ libzeropool-rs-node/src/lib.rs
@@ -1,5 +1,5 @@
 use libzeropool_rs::libzeropool::fawkes_crypto::backend::bellman_groth16::engines::Bn256;
-use libzeropool_rs::libzeropool::native::params::PoolBN256;
+use libzeropool_rs::libzeropool::native::params::{PoolBN256, PoolParams as PoolParamsTrait};
 use neon::prelude::*;
 
 mod client;
@@ -13,5 +13,7 @@ pub type Engine = Bn256;
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("fromBinary", params::from_binary)?;
+
     Ok(())
 }
--- libzeropool-rs-node/src/params.rs
+++ libzeropool-rs-node/src/params.rs
@@ -7,19 +7,13 @@ pub struct Params {
     pub inner: Parameters<Engine>,
 }
 
-pub fn from_binary(mut cx: FunctionContext) -> JsResult<Params> {
-    let mut input = cx.argument::<JsBuffer>(0)?.try_borrow().as_slice::<u8>();
-    let inner = Parameters::read(&mut input, true, true).map_err(|err| js_err!("{}", err))?;
+pub fn from_binary(mut cx: FunctionContext) -> JsResult<JsBox<Params>> {
+    let input = cx.argument::<JsBuffer>(0)?;
+    let guard = cx.lock();
+    let mut input = input.borrow(&guard).as_slice::<u8>();
+    let inner = Parameters::read(&mut input, true, true).unwrap();
 
-    Ok((Params { inner }).upcast())
+    Ok(cx.boxed(Params { inner }))
 }
 
-declare_types! {
-    pub class JsParams for Params {}
-}
-
-register_module!(mut m, {
-    m.export_function("fromBinary", from_binary)?;
-    m.export_class::<JsParams>("Params")?;
-    Ok(())
-});
+impl Finalize for Params {}
