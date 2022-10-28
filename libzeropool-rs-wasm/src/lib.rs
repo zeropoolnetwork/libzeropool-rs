0837e8e1f947b9d9b8096fb80cb58398bed2e238
Dmitry Vdovin <voidxnull@gmail.com>
2022-10-28 14:32:23 +0400

    Remove optional wee_alloc from wasm

--- libzeropool-rs-wasm/src/lib.rs
+++ libzeropool-rs-wasm/src/lib.rs
@@ -23,10 +23,6 @@ pub use crate::{
     ts_types::*,
 };
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[macro_use]
 mod utils;
 mod client;
