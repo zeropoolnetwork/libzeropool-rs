5090d9d81bacf545f495849d6c531bc15b7e9334
Dmitry Vdovin <voidxnull@gmail.com>
2023-01-05 22:17:56 +0400

    Finalize kvdb-persy

--- libs/kvdb-persy/src/lib.rs
+++ libs/kvdb-persy/src/lib.rs
@@ -18,7 +18,7 @@ fn decode_key(key: &str) -> Vec<u8> {
 }
 
 fn prefix_index(col: u32, prefix: &[u8]) -> String {
-    format!("p:{}:{}", col, encode_key(prefix))
+    format!("p:{}:{}", col, hex::encode(prefix))
 }
 
 fn id_index(col: u32) -> String {
@@ -61,14 +61,16 @@ impl PersyDatabase {
 impl KeyValueDB for PersyDatabase {
     fn get(&self, col: u32, key: &[u8]) -> std::io::Result<Option<DBValue>> {
         let key = encode_key(key);
-        let col = col.to_string();
+        let index_k_to_id = key_index(col);
+        let segment = col.to_string();
+
         let mut read_id = self
             .db
-            .get::<String, PersyId>(&col, &key)
+            .get::<String, PersyId>(&index_k_to_id, &key)
             .map_err(persy_to_io)?;
 
         if let Some(id) = read_id.next() {
-            let data = self.db.read(&col, &id).map_err(persy_to_io)?;
+            let data = self.db.read(&segment, &id).map_err(persy_to_io)?;
             Ok(data)
         } else {
             Ok(None)
@@ -116,12 +118,11 @@ impl KeyValueDB for PersyDatabase {
                             .map_err(persy_to_io)?;
                     }
 
-                    if tx
+                    if let Some(rec_id) = tx
                         .one::<String, PersyId>(&index_k_to_id, &key)
                         .map_err(persy_to_io)?
-                        .is_some()
                     {
-                        return Ok(());
+                        tx.delete(&segment, &rec_id).map_err(persy_to_io)?;
                     }
 
                     let rec_id = tx.insert(&segment, &value).map_err(persy_to_io)?;
@@ -162,9 +163,32 @@ impl KeyValueDB for PersyDatabase {
                     tx.delete(&segment, &rec_id).map_err(persy_to_io)?;
                 }
                 DBOp::DeletePrefix { col, prefix } => {
-                    // let prefix = encode_key(prefix);
-                    // let col = col.to_string();
-                    todo!()
+                    let prefix_index = prefix_index(col, &prefix);
+                    let segment = col.to_string();
+                    let index_k_to_id = key_index(col);
+                    let index_id_to_k = id_index(col);
+
+                    if prefix.is_empty() {
+                        tx.drop_segment(&segment).map_err(persy_to_io)?;
+                        // tx.drop_index(&index_k_to_id).map_err(persy_to_io)?;
+                        tx.drop_index(&index_id_to_k).map_err(persy_to_io)?;
+                        tx.create_segment(&segment).map_err(persy_to_io)?;
+                        continue;
+                    }
+
+                    let mut rec_ids = tx
+                        .range::<String, PersyId, _>(&prefix_index, ..)
+                        .map_err(persy_to_io)?
+                        .collect::<Vec<_>>();
+
+                    for (key, mut values) in rec_ids.drain(..) {
+                        let rec_id = values.next().unwrap();
+                        tx.remove::<String, PersyId>(&index_k_to_id, key, None)
+                            .map_err(persy_to_io)?;
+                        tx.remove::<PersyId, String>(&index_id_to_k, rec_id, None)
+                            .map_err(persy_to_io)?;
+                        tx.delete(&segment, &rec_id).map_err(persy_to_io)?;
+                    }
                 }
             }
         }
@@ -199,7 +223,10 @@ impl KeyValueDB for PersyDatabase {
         col: u32,
         prefix: &'a [u8],
     ) -> Box<dyn Iterator<Item = std::io::Result<DBKeyValue>> + 'a> {
-        // 1. Retrieve all PersyId for the given prefix.
+        if prefix.is_empty() {
+            return self.iter(col);
+        }
+
         let pairs = self
             .db
             .range::<String, PersyId, _>(&prefix_index(col, prefix), ..)
@@ -247,9 +274,9 @@ mod tests {
         &[255],
         &[255, 255],
         &[8],
+        b"03c69be41d0b7e40352fc85be1cd65eb03d40ef8427a0ca4596b1ead9a00e9fc",
+        b"04c00000000b7e40352fc85be1cd65eb03d40ef8427a0ca4596b1ead9a00e9fc",
     ];
-
-    const FILE_N: AtomicUsize = AtomicUsize::new(0);
 
     struct TestContext {
         file_name: String,
@@ -262,10 +289,16 @@ mod tests {
         }
     }
 
-    fn setup() -> TestContext {
+    fn new_file_name() -> String {
+        static FILE_N: AtomicUsize = AtomicUsize::new(0);
         let file_n = FILE_N.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-        let file_name = format!("test-{file_n}.persy");
+        format!("test-{file_n}.persy")
+    }
+
+    fn setup() -> TestContext {
+        let file_name = new_file_name();
         let _ = std::fs::remove_file(&file_name);
+        println!("{file_name}");
         let db = PersyDatabase::open(&file_name, PREFIXES).unwrap();
 
         TestContext { file_name, db }
@@ -279,9 +312,14 @@ mod tests {
         tx.put(0, &[2], &[2, 2, 2, 2]);
         ctx.db.write(tx).unwrap();
 
+        assert_eq!(ctx.db.get(0, &[1]).unwrap(), Some(vec![1, 1, 1, 1]));
+        assert_eq!(ctx.db.get(0, &[2]).unwrap(), Some(vec![2, 2, 2, 2]));
+        assert_eq!(ctx.db.get(0, &[3]).unwrap(), None);
+
         let results = ctx.db.iter(0).collect::<Result<Vec<_>, _>>().unwrap();
         assert_eq!(results.len(), 2);
     }
+
     #[test]
     pub fn test_put_and_get() {
         let ctx = setup();
@@ -305,6 +343,7 @@ mod tests {
         let ctx = setup();
         st::test_write_clears_buffered_ops(&ctx.db).unwrap();
     }
+
     #[test]
     pub fn test_iter() {
         let ctx = setup();
@@ -316,12 +355,6 @@ mod tests {
         let ctx = setup();
         st::test_iter_with_prefix(&ctx.db).unwrap();
     }
-
-    // #[test]
-    // pub fn test_io_stats() {
-    //     let ctx = setup();
-    //     st::test_io_stats(&ctx.db).unwrap();
-    // }
 
     #[test]
     pub fn test_delete_prefix() {
